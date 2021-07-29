ad08d2f95562160e15c195081195d1a21dff3b41
levan92 <lingevan0208@gmail.com>
2021-07-29 18:41:07 +0800

    tf embedder up

--- embedder_tf.py
+++ embedder_tf.py
@@ -1,271 +1,149 @@
-# vim: expandtab:ts=4:sw=4
-import os
-import errno
-import argparse
-import numpy as np
+import logging
+from pathlib import Path
+
 import cv2
+import numpy as np
 import tensorflow as tf
 
-def preprocess(np_image_bgr):
+log_level = logging.DEBUG
+logger = logging.getLogger('Embedder for Deepsort')
+logger.setLevel(log_level)
+handler = logging.StreamHandler()
+handler.setLevel(log_level)
+formatter = logging.Formatter('[%(levelname)s] [%(name)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+INPUT_WIDTH = 224
+
+def batch(iterable, bs=1):
+    l = len(iterable)
+    for ndx in range(0, l, bs):
+        yield iterable[ndx:min(ndx + bs, l)]
+
+def get_mobilenetv2_with_preproc(wts='imagenet'):
+    i = tf.keras.layers.Input([None, None, 3], dtype = tf.uint8)
+    x = tf.cast(i, tf.float32)
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
+
+    full_model = tf.keras.applications.mobilenet_v2.MobileNetV2(
+        input_shape=None, 
+        weights=str(wts),
+        classifier_activation=None, 
+    )
+    core_model = tf.keras.Model(full_model.input, full_model.layers[-2].output)
+
+    x = core_model(x)
+
+    model = tf.keras.Model(inputs=[i], outputs=[x])
+    model.summary()
+    return model
+
+class MobileNetv2_Embedder(object):
     '''
-    Preprocessing for embedder network
+    MobileNetv2_Embedder loads a Mobilenetv2 pretrained on Imagenet1000, with classification layer removed, exposing the bottleneck layer, outputing a feature of size 1280. 
 
-    Parameters
-    ----------
-    np_image_bgr : ndarray
-        (B x H x W x C) in BGR
+    Params
+    ------
+    - model_wts_path (optional, str) : path to mobilenetv2 model weights, defaults to the model file in ./mobilenetv2
+    - max_batch_size (optional, int) : max batch size for embedder, defaults to 16
+    - bgr (optional, Bool) : boolean flag indicating if input frames are bgr or not, defaults to True
 
-    Returns
-    -------
-    ndarray in ()
     '''
-    image_patches = []
-    for box in boxes:
-        tlwh = box['tlwh']
-        t, l, w, h = [tlwh[key] for key in 'tlwh']
-        y = l - h
-        x = t
-        rows = [x, y, w, h]
-        # patch = extract_image_patch(image, box, image_shape[:2])
-        # if patch is None:
-        #     print("WARNING: Failed to extract image patch: %s." % str(box))
-        #     patch = np.random.uniform(
-        #         0., 255., image_shape).astype(np.uint8)
-        image_patches.append(patch)
-    image_patches = np.asarray(image_patches)
+    def __init__(self, model_wts_path = None, max_batch_size = 16, bgr=True):
+        if model_wts_path is None:
+            model_wts_path = 'mobilenetv2_tf/mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_224.h5'
+        model_wts_path = Path(model_wts_path)
+        assert model_wts_path.is_file(),'Mobilenetv2 model path does not exists!'
 
-class DS_Embedder(object):
-    '''
-    DS_Embedder loads DS's default lightweight person reid network trained on various opensource reid datasets 
-    '''
-    # def __init__(self, model_dir=None):
-    def __init__(self, checkpoint_filename=None, input_name="images",
-                 output_name="features"):
-        self.session = tf.Session()
-        with tf.gfile.GFile(checkpoint_filename, "rb") as file_handle:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(file_handle.read())
-        tf.import_graph_def(graph_def, name="net")
-        self.input_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % input_name)
-        self.output_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % output_name)
+        self.model = get_mobilenetv2_with_preproc(wts=model_wts_path)
 
-        assert len(self.output_var.get_shape()) == 2
-        assert len(self.input_var.get_shape()) == 4
-        self.feature_dim = self.output_var.get_shape().as_list()[-1]
-        self.image_shape = self.input_var.get_shape().as_list()[1:]
+        self.max_batch_size = max_batch_size
+        self.bgr = bgr
 
-    def predict(self, np_image_bgr_batch, batch_size = 16):
-        out = np.zeros((len(data_x), self.feature_dim), np.float32)
-        _run_in_batches(
-            lambda x: self.session.run(self.output_var, feed_dict=x),
-            {self.input_var: data_x}, out, batch_size)
-        return out
+        logger.info('MobileNetV2 Embedder (tf) for Deep Sort initialised')
+        logger.info(f'- max batch size: {self.max_batch_size}')
+        logger.info(f'- expects BGR: {self.bgr}')
 
+        zeros = np.zeros((100, 100, 3), dtype=np.uint8)
+        self.predict([zeros, zeros]) #warmup
 
-def _run_in_batches(f, data_dict, out, batch_size):
-    data_len = len(out)
-    num_batches = int(data_len / batch_size)
+    def preprocess(self, np_image):
+        '''
+        Parameters
+        ----------
+        np_image : ndarray
+            (H x W x C)
 
-    s, e = 0, 0
-    for i in range(num_batches):
-        s, e = i * batch_size, (i + 1) * batch_size
-        batch_data_dict = {k: v[s:e] for k, v in data_dict.items()}
-        out[s:e] = f(batch_data_dict)
-    if e < len(out):
-        batch_data_dict = {k: v[e:] for k, v in data_dict.items()}
-        out[e:] = f(batch_data_dict)
+        Returns
+        -------
+        TF Tensor
 
-
-def extract_image_patch(image, bbox, patch_shape):
-    """Extract image patch from bounding box.
-    Parameters
-    ----------
-    image : ndarray
-        The full image.
-    bbox : array_like
-        The bounding box in format (x, y, width, height).
-    patch_shape : Optional[array_like]
-        This parameter can be used to enforce a desired patch shape
-        (height, width). First, the `bbox` is adapted to the aspect ratio
-        of the patch shape, then it is clipped at the image boundaries.
-        If None, the shape is computed from :arg:`bbox`.
-    Returns
-    -------
-    ndarray | NoneType
-        An image patch showing the :arg:`bbox`, optionally reshaped to
-        :arg:`patch_shape`.
-        Returns None if the bounding box is empty or fully outside of the image
-        boundaries.
-    """
-    bbox = np.array(bbox)
-    if patch_shape is not None:
-        # correct aspect ratio to patch shape
-        target_aspect = float(patch_shape[1]) / patch_shape[0]
-        new_width = target_aspect * bbox[3]
-        bbox[0] -= (new_width - bbox[2]) / 2
-        bbox[2] = new_width
-
-    # convert to top left, bottom right
-    bbox[2:] += bbox[:2]
-    bbox = bbox.astype(np.int)
-
-    # clip at image boundaries
-    bbox[:2] = np.maximum(0, bbox[:2])
-    bbox[2:] = np.minimum(np.asarray(image.shape[:2][::-1]) - 1, bbox[2:])
-    if np.any(bbox[:2] >= bbox[2:]):
-        return None
-    sx, sy, ex, ey = bbox
-    image = image[sy:ey, sx:ex]
-    image = cv2.resize(image, tuple(patch_shape[::-1]))
-    return image
-
-
-class ImageEncoder(object):
-
-    def __init__(self, checkpoint_filename, input_name="images",
-                 output_name="features"):
-        self.session = tf.Session()
-        with tf.gfile.GFile(checkpoint_filename, "rb") as file_handle:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(file_handle.read())
-        tf.import_graph_def(graph_def, name="net")
-        self.input_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % input_name)
-        self.output_var = tf.get_default_graph().get_tensor_by_name(
-            "net/%s:0" % output_name)
-
-        assert len(self.output_var.get_shape()) == 2
-        assert len(self.input_var.get_shape()) == 4
-        self.feature_dim = self.output_var.get_shape().as_list()[-1]
-        self.image_shape = self.input_var.get_shape().as_list()[1:]
-
-    def __call__(self, data_x, batch_size=32):
-        out = np.zeros((len(data_x), self.feature_dim), np.float32)
-        _run_in_batches(
-            lambda x: self.session.run(self.output_var, feed_dict=x),
-            {self.input_var: data_x}, out, batch_size)
-        return out
-
-
-def create_box_encoder(model_filename, input_name="images",
-                       output_name="features", batch_size=32):
-    image_encoder = ImageEncoder(model_filename, input_name, output_name)
-    image_shape = image_encoder.image_shape
-
-    def encoder(image, boxes):
-        image_patches = []
-        for box in boxes:
-            tlwh = box['tlwh']
-            t, l, w, h = [tlwh[key] for key in 'tlwh']
-            y = l - h
-            x = t
-            rows = [x, y, w, h]
-            patch = extract_image_patch(image, rows, image_shape[:2])
-            # patch = extract_image_patch(image, box, image_shape[:2])
-            if patch is None:
-                print("WARNING: Failed to extract image patch: %s." % str(box))
-                patch = np.random.uniform(
-                    0., 255., image_shape).astype(np.uint8)
-            image_patches.append(patch)
-        image_patches = np.asarray(image_patches)
-        return image_encoder(image_patches, batch_size)
-
-    return encoder
-
-
-def generate_detections(encoder, mot_dir, output_dir, detection_dir=None):
-    """Generate detections with features.
-    Parameters
-    ----------
-    encoder : Callable[image, ndarray] -> ndarray
-        The encoder function takes as input a BGR color image and a matrix of
-        bounding boxes in format `(x, y, w, h)` and returns a matrix of
-        corresponding feature vectors.
-    mot_dir : str
-        Path to the MOTChallenge directory (can be either train or test).
-    output_dir
-        Path to the output directory. Will be created if it does not exist.
-    detection_dir
-        Path to custom detections. The directory structure should be the default
-        MOTChallenge structure: `[sequence]/det/det.txt`. If None, uses the
-        standard MOTChallenge detections.
-    """
-    if detection_dir is None:
-        detection_dir = mot_dir
-    try:
-        os.makedirs(output_dir)
-    except OSError as exception:
-        if exception.errno == errno.EEXIST and os.path.isdir(output_dir):
-            pass
+        '''
+        if self.bgr:
+            np_image_rgb = np_image[...,::-1]
         else:
-            raise ValueError(
-                "Failed to created output directory '%s'" % output_dir)
+            np_image_rgb = np_image
+        np_image_rgb = cv2.resize(np_image_rgb, (INPUT_WIDTH, INPUT_WIDTH))
+        return tf.convert_to_tensor(np_image_rgb)
 
-    for sequence in os.listdir(mot_dir):
-        print("Processing %s" % sequence)
-        sequence_dir = os.path.join(mot_dir, sequence)
+    def predict(self, np_images):
+        '''
+        batch inference
 
-        image_dir = os.path.join(sequence_dir, "img1")
-        image_filenames = {
-            int(os.path.splitext(f)[0]): os.path.join(image_dir, f)
-            for f in os.listdir(image_dir)}
+        Params
+        ------
+        np_images : list of ndarray
+            list of (H x W x C), bgr or rgb according to self.bgr
+        
+        Returns
+        ------
+        list of features (np.array with dim = 1280)
 
-        detection_file = os.path.join(
-            detection_dir, sequence, "det/det.txt")
-        detections_in = np.loadtxt(detection_file, delimiter=',')
-        detections_out = []
+        '''
+        all_feats = []
 
-        frame_indices = detections_in[:, 0].astype(np.int)
-        min_frame_idx = frame_indices.astype(np.int).min()
-        max_frame_idx = frame_indices.astype(np.int).max()
-        for frame_idx in range(min_frame_idx, max_frame_idx + 1):
-            print("Frame %05d/%05d" % (frame_idx, max_frame_idx))
-            mask = frame_indices == frame_idx
-            rows = detections_in[mask]
+        preproc_imgs = [ self.preprocess(img) for img in np_images ]
 
-            if frame_idx not in image_filenames:
-                print("WARNING could not find image for frame %d" % frame_idx)
-                continue
-            bgr_image = cv2.imread(
-                image_filenames[frame_idx], cv2.IMREAD_COLOR)
-            features = encoder(bgr_image, rows[:, 2:6].copy())
-            detections_out += [np.r_[(row, feature)] for row, feature
-                               in zip(rows, features)]
+        for this_batch in batch(preproc_imgs, bs=self.max_batch_size):
+            this_batch = tf.stack(this_batch, axis=0)
+            output = self.model(this_batch)
+            all_feats.extend(output.numpy())
 
-        output_filename = os.path.join(output_dir, "%s.npy" % sequence)
-        np.save(
-            output_filename, np.asarray(detections_out), allow_pickle=False)
+        return all_feats
 
 
-def parse_args():
-    """Parse command line arguments.
-    """
-    parser = argparse.ArgumentParser(description="Re-ID feature extractor")
-    parser.add_argument(
-        "--model",
-        default="resources/networks/mars-small128.pb",
-        help="Path to freezed inference graph protobuf.")
-    parser.add_argument(
-        "--mot_dir", help="Path to MOTChallenge directory (train or test)",
-        required=True)
-    parser.add_argument(
-        "--detection_dir", help="Path to custom detections. Defaults to "
-        "standard MOT detections Directory structure should be the default "
-        "MOTChallenge structure: [sequence]/det/det.txt", default=None)
-    parser.add_argument(
-        "--output_dir", help="Output directory. Will be created if it does not"
-        " exist.", default="detections")
-    return parser.parse_args()
+if __name__ == '__main__':
+    import cv2
+    import numpy as np
+    import time
+    impath = '/media/dh/HDD/sample_data/images/cute_doggies.jpg'
+    auba = cv2.imread(impath)
+    
+    tic = time.time()
+    emb = MobileNetv2_Embedder(max_batch_size=16)
+    toc = time.time()
+    print(f'loading time: {toc - tic:0.4f}s')
 
+    # aubas = [auba] * 16
+    # feats = emb.predict(aubas)
+    # print(np.shape(feats))
 
-def main():
-    args = parse_args()
-    encoder = create_box_encoder(args.model, batch_size=32)
-    generate_detections(encoder, args.mot_dir, args.output_dir,
-                        args.detection_dir)
-
-
-if __name__ == "__main__":
-    main()
+    bses = [1,16]
+    reps = 100
+    for bs in bses:
+        aubas = [auba] * bs
+        dur = 0
+        for _ in range(reps):
+            tic = time.time()
+            feats = emb.predict(aubas)
+            toc = time.time()
+            dur += toc - tic
+        print(np.shape(feats))
+        print(f'inference BS{bs} avrg time: {dur/reps:0.4f}s')
---
+++ tests/test_embedder.py
@@ -0,0 +1,36 @@
+import unittest
+
+def test_embedder_generic(model, thresh=10):
+    import cv2
+    import numpy as np
+
+    img = cv2.imread('tests/smallapple.jpg')
+    small_angle = 1
+    image_center = tuple(np.array(img.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, small_angle, 1.0)
+    img_rot = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR, borderValue=(255,255,255))
+
+    img2 = cv2.imread('tests/rock.jpg')
+
+    emb = model(max_batch_size=4)
+    a,b,c = emb.predict([img, img_rot, img2])
+
+    small = np.linalg.norm(a-b)
+    large = np.linalg.norm(a-c)
+
+    print(f'close: {small} vs diff: {large}')
+    assert small < thresh,f'Small: {small} not small enough'
+    assert large > thresh,f'Large: {large} not large enough'
+    return True
+
+class TestModule(unittest.TestCase):
+    def test_embedder_torch(self):
+        from embedder_pytorch import MobileNetv2_Embedder
+        return test_embedder_generic(MobileNetv2_Embedder)
+
+    def test_embedder_tf(self):
+        from embedder_tf import MobileNetv2_Embedder
+        return test_embedder_generic(MobileNetv2_Embedder)
+
+if __name__ == '__main__':
+    unittest.main()
